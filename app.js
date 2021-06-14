e7b74934259b26c62c51d1fb41e75be88fd79572
Yiting Liao <tim80411@gmail.com>
2021-06-14 15:11:42 +0800

    feat: connect to database

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const handlebars = require('express-handlebars')
+const db = require('./models')
 
 const port = 3000
 
@@ -8,6 +9,7 @@ app.engine('handlebars', handlebars({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
 app.listen(port, () => {
+  db.sequelize.sync() // 同步資料庫
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
