ef3988eb70286accddbc368a73a0fa6aa4481363
Yiting Liao <tim80411@gmail.com>
2021-06-21 17:14:14 +0800

    feat: add category select in edit page using helper

--- app.js
+++ app.js
@@ -15,7 +15,10 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+app.engine('handlebars', handlebars({
+  defaultLayout: 'main',
+  helpers: require('./config/handlebars-helpers')
+}))
 app.set('view engine', 'handlebars')
 
 app.use(express.urlencoded({ extended: true }))
