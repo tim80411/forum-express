615bc7a181154dcae3756d5a0e2ad833df13ce7e
Yiting Liao <tim80411@gmail.com>
2021-06-22 16:42:18 +0800

    feat: Using moment to format createdAt time display

--- config/handlebars-helpers.js
+++ config/handlebars-helpers.js
@@ -1,8 +1,14 @@
+const moment = require('moment')
+
 module.exports = {
   ifCond: function (a, b, options) {
     if (a === b) {
       return options.fn(this)
     }
     return options.inverse(this)
+  },
+
+  momentFromNow: function (a) {
+    return moment(a).fromNow()
   }
 }
