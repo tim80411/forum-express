c0418217c80eb8d25ce5f211e62dd7d0fe9d700a
Yiting Liao <tim80411@gmail.com>
2021-06-23 01:33:24 +0800

    feat: A19-Q1 User Profile
    1. set up sequelize migration & modify model to add image column
    2. add editUser & user page
    3. add Read user profile & Update user profile feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,7 @@
 const bcrypt = require('bcryptjs')
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
+
 const db = require('../models')
 const User = db.User
 
@@ -45,6 +48,66 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+
+  getUser: (req, res) => {
+    const id = req.user.id
+
+    return User.findByPk(id)
+      .then(user => {
+        user = user.toJSON()
+
+        return res.render('user', { user })
+      })
+  },
+
+  editUser: (req, res) => {
+    const id = req.user.id
+
+    return User.findByPk(id)
+      .then(user => {
+        user = user.toJSON()
+
+        return res.render('editUser', { user })
+      })
+  },
+
+  putUser: (req, res) => {
+    const id = req.user.id
+    const { name } = req.body
+    const { file } = req
+
+    if (!name) {
+      req.flash('error_messages', '姓名不得為空')
+      return res.redirect(`/users/${id}/edit`)
+    }
+
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID)
+      imgur.upload(file.path, (err, img) => {
+        return User.findByPk(id)
+          .then(user => {
+            return user.update({
+              name,
+              image: img ? img.data.link : user.img
+            })
+          })
+          .then(user => {
+            return res.redirect(`/users/${user.id}`)
+          })
+      })
+    } else {
+      return User.findByPk(id)
+        .then(user => {
+          return user.update({
+            name,
+            image: user.img
+          })
+        })
+        .then(user => {
+          return res.redirect(`/users/${user.id}`)
+        })
+    }
   }
 }
 
