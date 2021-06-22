7e346914cb6d904cf86ace18ba47c57df382c132
Yiting Liao <tim80411@gmail.com>
2021-06-22 13:32:48 +0800

    feat: add READ the restaurant feature

--- controllers/restController.js
+++ controllers/restController.js
@@ -6,17 +6,25 @@ const restController = {
   getRestaurants: (req, res) => {
     return Restaurant.findAll({ include: Category })
       .then(restaurants => {
-        // 記得刪除
-        console.log('@@@@@restaurants', restaurants[0])
         const data = restaurants.map(r => ({
           ...r.dataValues,
           description: r.dataValues.description.substring(0, 50),
           categoryName: r.Category.name
         }))
 
-        return res.render('restaurants', {restaurants: data})
+        return res.render('restaurants', { restaurants: data })
       })
+  },
 
+  getRestaurant: (req, res) => {
+    const id = req.params.id
+
+    return Restaurant.findByPk(id, { include: Category })
+      .then(restaurant => {
+        return res.render('restaurnat', {
+          restaurant: restaurant.toJSON()
+        })
+      })
   }
 }
 
