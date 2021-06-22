05a26271e20d47bcb6a2f7d7ddd421aa17bb90c5
Yiting Liao <tim80411@gmail.com>
2021-06-22 17:12:04 +0800

    feat: Add Delete comment feature for admin

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -12,6 +12,16 @@ const commentController = {
     }).then(comment => {
       return res.redirect(`/restaurants/${req.body.restaurantId}`)
     })
+  },
+
+  deleteComment: (req, res) => {
+    return Comment.findByPk(req.params.id)
+      .then(comment => {
+        return comment.destroy()
+      })
+      .then(comment => {
+        return res.redirect(`/restaurants/${comment.RestaurantId}`)
+      })
   }
 }
 
