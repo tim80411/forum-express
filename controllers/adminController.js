fe49bd615a246b6d5e93eb1131e61328f7d2957a
Yiting Liao <tim80411@gmail.com>
2021-06-28 16:29:48 +0800

    refactor: modularize adminController to adminService

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -6,17 +6,14 @@ const Restaurant = db.Restaurant
 const User = db.User
 const Category = db.Category
 
+const adminService = require('../services/adminService')
+
 
 const adminController = {
   getRestaurants: (req, res) => {
-    return Restaurant.findAll({
-      raw: true,
-      nest: true,
-      include: Category
+    return adminService.getRestaurants(req, res, (data) => {
+      return res.render('admin/restaurants',  data)
     })
-      .then(restaurants => {
-        return res.render('admin/restaurants', { restaurants })
-      })
   },
 
   createRestaurant: (req, res) => {
--- controllers/api/adminController.js
+++ controllers/api/adminController.js
@@ -1,17 +1,10 @@
-const db = require('../../models')
-const Restaurant = db.Restaurant
-const Category = db.Category
+const adminService = require('../../services/adminService')
 
 const adminController = {
   getRestaurants: (req, res) => {
-    return Restaurant.findAll({
-      raw: true,
-      nest: true,
-      include: Category
+    return adminService.getRestaurants(req, res, (data) => {
+      return res.json(data)
     })
-      .then(restaurants => {
-        return res.json({ restaurants })
-      })
   },
 }
 
